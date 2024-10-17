0a3188b64285fd009d704a5379b588ff2b9c888f
Remi Nilsen <remi.ulv.nilsen@hotmail.com>
2024-10-17 13:31:43 +0200

    begynte med scrimba oppgaver

---
+++ forskjellige oppgaver/lagettall.js
@@ -0,0 +1,17 @@
+function regn(){
+
+    let tall = document.getElementById("tall").value
+    let double = tall*2
+    let trippel = tall*3
+    let half = tall/2
+    let kvadrat = tall*tall
+    
+    document.getElementById("utskrift").innerHTML = `taller er ${tall} det dobble er ${double}       `
+
+
+
+}
+
+
+
+
