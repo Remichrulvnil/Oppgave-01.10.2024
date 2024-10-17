02daf6eee4bdc2b587761455ef923515cea26fed
Remi Nilsen <remi.ulv.nilsen@hotmail.com>
2024-10-17 14:23:14 +0200

    lagde egen counter

---
+++ scrimba_increment_counter_mario/counter_av_remi/index.js
@@ -0,0 +1,10 @@
+let count=0
+
+
+function trykke() {
+    count = count + 1
+    //trykk.innertext = count
+    console.log(count)
+    document.getElementById("talletsomendrerseg").innerText= count
+
+}
